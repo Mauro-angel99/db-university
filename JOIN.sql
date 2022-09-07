8a264e41b0b1870d9597de7cd40d7e52f5fd0de2
mauro-angel99 <angelottimauro@gmail.com>
2022-09-07 16:55:26 +0200

    JOIN 3

--- JOIN.sql
+++ JOIN.sql
@@ -15,6 +15,15 @@ ON `degrees`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
+FROM `courses`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `teachers`.`id` = 44;
+
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
